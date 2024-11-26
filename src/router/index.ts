23a5de29d9219d16ecae8cd49af0ea970e7a2c01
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-11-26 16:16:51 +0100

    feat(HabitsView) : Création de la View et configuration des Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUpView from '../views/SignUpView.vue'
+import HabitsView from '../views/HabitsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUpView,
+    },
+    {
+      path: '/habits',
+      name: 'habits',
+      component: HabitsView,
     },
   ],
 })
