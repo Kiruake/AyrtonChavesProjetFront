6d1a59159e5f1d17b1d5f4ab9e7fe6a8ca16581c
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-11-26 11:04:47 +0100

    feat(fix ESLint erreurs)

--- ProjetFront/eslint.config.js
+++ ProjetFront/eslint.config.js
@@ -16,4 +16,11 @@ export default [
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   skipFormatting,
+  
+  {
+rules: {
+  'vue/multi-word-component-names': 'off',
+  },
+  },
 ]
+
