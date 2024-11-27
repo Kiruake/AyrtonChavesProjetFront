e25257a0b2937c549ab55053b3fb2422fd579442
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-11-27 09:32:30 +0100

    feat(NewColors): change variables and SCSS

---
+++ src/views/AddHabitsView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+
+import CardAddHabit from '../components/CardAddHabit.vue'
+
+
+</script>
+
+<template>
+  <main>
+
+    
+    <h1 class="title">ajouter une habitude</h1>
+  
+
+<CardAddHabit/>
+
+  </main>
+</template>
+
+<style lang="scss">
+
+.title {
+  font-family: $fontTitleFamily;
+  text-align: center;
+  color: $primaryColor;
+  font-size: 2rem
+}
+
+</style>
