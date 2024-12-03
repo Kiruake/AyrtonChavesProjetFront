d4d1c62aa1e0271e70d862ad1792bea608945fa1
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-12-03 10:56:21 +0100

    feat(PageBlog): Creation Index et [slug] -> Posts avec Sanity

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { SanityDocument } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+const  route  = useRoute();
+
+const { data: post } = useSanityQuery(POST_QUERY, {slug: route.params.slug}
+);
+//const { projectId, dataset } = useSanity().client.config();
+//const urlFor = (source: SanityImageSource) =>
+ // projectId && dataset
+  //  ? imageUrlBuilder({ projectId, dataset }).image(source)
+   // : null;
+</script>
+
+<template>
+  <main>
+    <div>
+     <h1> Une page d'article de blog :{{route.params.slug}}</h1>
+      <h1>Contenu du post : {{ post }}</h1>
+    </div>
+  </main>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { SanityDocument } from "@sanity/client";
+
+const POSTS_QUERY = groq`*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, image, title, slug, publishedAt}`;
+
+const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY);
+</script>
+
+<template>
+
+    <div>
+       <h1> page de Blog</h1>
+{{posts}}
+    </div>
+</template>
+
+<style lang="scss"></style>
