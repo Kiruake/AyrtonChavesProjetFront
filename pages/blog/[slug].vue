85652a5e71eec0e56df9fdddf2d5b79d3c9dce2d
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-12-03 13:38:06 +0100

    feat(Blog): Mise en place des données dans le slug et l'index.

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
+
 import type { SanityDocument } from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 const  route  = useRoute();
 
-const { data: post } = useSanityQuery(POST_QUERY, {slug: route.params.slug}
-);
-//const { projectId, dataset } = useSanity().client.config();
-//const urlFor = (source: SanityImageSource) =>
- // projectId && dataset
-  //  ? imageUrlBuilder({ projectId, dataset }).image(source)
-   // : null;
+const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, {slug: route.params.slug});
+
 </script>
 
 <template>
   <main>
-    <div>
-     <h1> Une page d'article de blog :{{route.params.slug}}</h1>
-      <h1>Contenu du post : {{ post }}</h1>
+    <div class="blog-slug" v-if ="post">
+      <h1 class="blog-slug__title" >{{ post.title }}</h1>
+      <p class="blog-slug__date">{{ post.publishedAt }}</p>
+      <SanityContent class="blog-slug__content" :blocks="post.body"/>
+      <SanityImage v-if="post.image" :asset-id="post.image.asset._ref" />
     </div>
+
+   
   </main>
 </template>
+
+<style lang="scss">
+
+.blog-slug {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 1rem;
+  &__title {
+    font-family: $fontTitleFamily;
+    color: $primaryColor;
+    text-align: center;
+    margin-bottom: 5px;
+    font-size: $fontTitleSize;
+  }
+  &__date {
+    font-size: 1.2rem;
+  }
+  &__content {
+    font-size: 1.6rem;
+  }
+}
+
+
+</style>
